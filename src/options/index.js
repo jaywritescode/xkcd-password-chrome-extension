912e7ce44babd4a8a887ba80d366c7df1ac4c873
Jay Harris <jaywritescode@users.noreply.github.com>
2018-05-28 21:46:19 -0400

    show error if javascript syntax error

--- src/options/index.js
+++ src/options/index.js
@@ -1,9 +1,11 @@
 import './index.html';
 import './index.scss';
+import * as Babel from '@babel/standalone';
 
 const transformInput = document.getElementById('transform-function');
 const urlInput = document.getElementById('url');
 const saveButton = document.getElementById('save-button');
+const syntaxErrorHelp = document.getElementById('syntax-error');
 
 const noop = function() {};
 
@@ -20,11 +22,31 @@ window.onload = () => {
 };
 
 saveButton.onclick = () => {
-  const settings = {
-    code: transformInput.value,
-    url: urlInput.value,
-  };
+  try {
+    Babel.transform(transformInput.value, { presets: [] });
 
-  console.log('settings: %O', settings);
-  chrome.storage.local.set(settings, noop);
+    hideSyntaxError();
+
+    const settings = {
+      code: transformInput.value,
+      url: urlInput.value,
+    };
+
+    chrome.storage.local.set(settings, noop);
+  }
+  catch (e) {
+    showSyntaxError();
+  }
 };
+
+function showSyntaxError() {
+  transformInput.classList.add('is-danger');
+  transformInput.nextElementSibling.classList.replace('is-invisible', 'has-danger-text');
+  syntaxErrorHelp.classList.replace('is-invisible', 'is-danger');
+}
+
+function hideSyntaxError() {
+  transformInput.classList.remove('is-danger');
+  transformInput.nextElementSibling.classList.replace('has-danger-text', 'is-invisible');
+  syntaxErrorHelp.classList.replace('is-danger', 'is-invisible');
+}
